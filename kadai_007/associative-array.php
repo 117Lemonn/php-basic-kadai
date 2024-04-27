00990dd6b25ae06c8e4f68d094c54a59f13d4d8e
117Lemonn <156330476+117Lemonn@users.noreply.github.com>
2024-04-28 00:16:26 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html long="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+
+</head>
+
+<body>
+    <P>
+        <?php
+        //連想配列に値を代入する
+        $personal_data=['name'=>'onion','price'=>200,'weight'=>160];
+
+        //連想配列の値を出力する
+        print_r($personal_data);
+
+        
+        ?>
+
+</p>
+</body>
+</html>
